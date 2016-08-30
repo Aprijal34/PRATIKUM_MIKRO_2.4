c2ba9111f1f3aa39a1b8e356ac2cde6cae780175
Aprijal <iskandaraprijal@gmail.com>
2016-08-30 11:18:29 +0700

    Add files via upload

---
+++ PRAKTIKUM_MIKRO 2.4.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A0,INPUT );
+digitalWrite(A0,HIGH);
+pinMode(12,INPUT );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(digitalRead(A0)==LOW)
+{
+  digitalWrite(12,HIGH);
+  }
+  else
+  {
+    digitalWrite(12,LOW);
+    }
+}
